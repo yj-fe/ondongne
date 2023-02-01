0c2a53eb018ab7ada7afa865d8fb135665e4803f
yj-fe <coder2023y@gmail.com>
2023-02-01 12:12:49 +0900

    c

--- src/components/Main/Main/MainBestCoupon/MainBestCoupon.jsx
+++ src/components/Main/Main/MainBestCoupon/MainBestCoupon.jsx
@@ -3,42 +3,100 @@ import bestcoupon from 'assets/data/bestcoupon'
 import newmarket from 'assets/data/newmarket'
 import * as L from 'components/commonUi/Layout';
 import * as T from 'components/commonUi/Text';
-
+import { getLastGroupItemList } from 'service/main';
 import {MainBestCouponBody,MainBestCouponContent,MainBestCouponTop,TopAll,TopTitle,ContentCoupon,ContentCouponDiv,ContentDrive,ContentImg,ContentMarket,ContentPick,ContentProduct} from './MainBestCouponStyle'
-
+import { CursorDiv } from 'components/Common/LayoutPageStyle';
+import LoadingBar from 'components/commonUi/LoadingBar';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 function MainBestCoupon() {
   let [item] = useState(newmarket)
 
+  const navigate = useNavigate();
+  const local = useSelector(state => state.local);
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState([]);
+
+
+  const getItem = async () => {
+    const response = await getLastGroupItemList(local);
+    if (response && response.data) {
+      const { data } = response.data;
+      setList(data.items);
+      setTimeout(() => {
+        setLoading(false);
+      }, 1000)
+    }
+  }
+
+  const router = () => {
+    navigate(
+      "/collections",
+      {
+        state:
+        {
+          // type: ,
+          title: "우리동네 인기 쿠폰",
+          list: list
+        }
+      }
+    )
+  }
+
+  useEffect(() => {
+    setLoading(true)
+    getItem();
+  }, [])
+
   return (
-    <div>
-        <L.FlexRows _content='space-between' _items='center' _padding='0px 20px 0px 0px'>
+    <CursorDiv>
+        <L.FlexRows _content='space-between' _items='center' _padding='0px 20px 0px 20px'>
           <T.Text _size={18} _weight={700} _color='black'>우리동네 인기 쿠폰</T.Text>
-          <T.Text _size={14} _weight={500} _color='blue'>전체 보기</T.Text>
+          <T.Text 
+            _size={14} 
+            _weight={500} 
+            _color='blue'
+            onClick={router}
+          >
+            전체 보기
+          </T.Text>
         </L.FlexRows>
 
         <L.FlexRowsCP>
-          <MainBestCouponContent>
-            {
+          {/* <MainBestCouponContent> */}
+          {
+            loading && <LoadingBar />
+          }
+          {
+            !loading &&
+            list.length > 0 &&
+            <L.GridContainer>
+              {
+                list.map((item, index) => (
+                  <div
+                    key={index}
+                    onClick={() => navigate(``)}
+                  >
+                    <BestCouponCard item={item} />
+                  </div>
+                ))
+              }
+            </L.GridContainer>
+          }
+            {/* {
               item.map((a, i)=>{
                 return(
                   <BestCouponCard item={item[i]} i={i}/>
                   )
                 })
-              }
-            {
-              item.map((a, i)=>{
-                return(
-                  <BestCouponCard item={item[i]} i={i}/>
-                  )
-                })
-              }
-          </MainBestCouponContent>
+              } */}
+          {/* </MainBestCouponContent> */}
         </L.FlexRowsCP>
 
 
 
-    </div>
+    </CursorDiv>
   )
 }
 
--- src/pages/main/MainPage/MainPage.jsx
+++ src/pages/main/MainPage/MainPage.jsx
@@ -54,9 +54,9 @@ function MainPage() {
           </L.Container>
 
           {/* 우리동네 인기 쿠폰 */}
-          {/* <L.Contents _padding='20px 0px 20px 20px'>
-                <MainBestCoupon />
-              </L.Contents> */}
+          {/* <L.Contents _padding='20px 0px 20px 0px'>
+              <MainBestCoupon />
+            </L.Contents> */}
 
           {/* 공동구매 마지막 찬스 */}
           <L.Contents _padding="20px 0px 20px 0px">
